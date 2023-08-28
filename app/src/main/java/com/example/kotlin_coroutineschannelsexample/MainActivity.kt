90a1e528d5ecfee5fa1845a2402d52213433647e
George <siodo@mail.ru>
2023-08-28 14:55:07 +0300

    first commit

--- app/src/main/java/com/example/kotlin_coroutineschannelsexample/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_coroutineschannelsexample/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.kotlin_coroutineschannelsexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlin_coroutineschannelsexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+
+
+
+        }
     }
 }
